86d74abb2384132ca18d68bebd7152c5b115be2b
daveKidd <david.kidd@lambdaschool.com>
2021-02-22 12:00:41 -0700

    ketchup

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,16 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res) => {
+const restricted = (req,res,next)=>{
+  if(req.session && req.session.user){
+    next()
+  }else{
+    res.status(401).json("unauthorized")
+  }
+}
+
+
+router.get("/", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
