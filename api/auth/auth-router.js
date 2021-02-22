fa98f694b93ca76c5b4d10efbc3eeebcb76ec190
daveKidd <david.kidd@lambdaschool.com>
2021-02-22 11:17:03 -0700

    ketchup

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../users/users-model.js")
+const bcrypt = require("bcryptjs")
 
 const checkPayload = (req,res,next)=>{
     if(!req.body.username || !req.body.password){
@@ -12,15 +13,25 @@ const checkPayload = (req,res,next)=>{
 
 const checkUserInDb = async (req,res,next)=>{
     try{
-        const user = await User.findBy({username: req.body.username})
-        console.log(user)
+        const rows = await User.findBy({username: req.body.username})
+        if(!rows.length){
+            next()
+        }else{
+            res.status(401).json("Username already exists")
+        }
     }catch(e){
         res.status(500).json(`Server error: ${e}`)
     }
 }
 
-router.post("/register", checkPayload, checkUserInDb, (req,res)=>{
-    
+router.post("/register", checkPayload, checkUserInDb, async (req,res)=>{
+    try{
+        const hash = bcrypt.hashSync(req.body.password, 10) //2^10 
+        const newUser = await User.add({username: req.body.username, password:hash})
+        res.status(201).json(newUser)
+    }catch(e){
+        res.status(500).json({message: e.message})
+    }
 })
 
 router.post("/login", (req,res)=>{
