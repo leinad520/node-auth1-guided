cfbbe34fdb889509f82e9ab4326cdc7bf8dc93d1
daveKidd <david.kidd@lambdaschool.com>
2021-02-22 10:58:55 -0700

    ketchup

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,26 @@
 const express = require("express")
 const router = express.Router()
+const User = require("../users/users-model.js")
 
-router.post("/register", (req,res)=>{
-    console.log("Registering!")
+const checkPayload = (req,res,next)=>{
+    if(!req.body.username || !req.body.password){
+        res.status(401).json("Username or password missing")
+    }else{
+        next()
+    }
+}
+
+const checkUserInDb = async (req,res,next)=>{
+    try{
+        const user = await User.findBy({username: req.body.username})
+        console.log(user)
+    }catch(e){
+        res.status(500).json(`Server error: ${e}`)
+    }
+}
+
+router.post("/register", checkPayload, checkUserInDb, (req,res)=>{
+    
 })
 
 router.post("/login", (req,res)=>{
